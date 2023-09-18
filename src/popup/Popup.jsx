5870f1430702e9517fb145d8e3f0fb38a6116908
Ghanshyam <gc.chou17@gmail.com>
2023-09-18 19:07:33 +0530

    added badge

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -2,54 +2,76 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-import { Button, Container, Form, ListGroup } from 'react-bootstrap';
+import { Button, Container, Form, ListGroup, Badge } from 'react-bootstrap';
 
 const Popup = () => {
   const [date, setDate] = React.useState('');
+  const [eventName, setEventName] = React.useState('');
   const [dates, setDates] = React.useState([]);
+  const [selectedDateIndex, setSelectedDateIndex] = React.useState(null);
 
   const handleSubmit = () => {
-    setDates([...dates, date]);
-    setDate('');
+    if (date && eventName) {
+      setDates([...dates, { date, eventName }]);
+      setDate('');
+      setEventName('');
+    }
   };
 
   const handleDelete = (index) => {
     setDates(dates.filter((_, i) => i !== index));
+    if (index === selectedDateIndex) {
+      setSelectedDateIndex(null);
+    }
   };
 
   const handleSelect = (index) => {
-    // Get the selected date.
-    const selectedDate = dates[index];
-  
-    // Calculate the remaining days for the selected date.
-    const remainingDays = Math.ceil((new Date(selectedDate) - new Date()) / (1000 * 60 * 60 * 24));
-
-    // Send a message to the background script to update the badge value.
-    chrome.runtime.sendMessage({
-      type: 'updateBadgeValue',
-      remainingDays: remainingDays,
-    });
+    setSelectedDateIndex(index);
   };
 
   return (
     <Container>
       <h1 className="mt-4 mb-3">Days Tracker</h1>
       <Form className="mb-3">
+        <Form.Group controlId="eventName">
+          <Form.Label>Event Name</Form.Label>
+          <Form.Control
+            type="text"
+            value={eventName}
+            onChange={(e) => setEventName(e.target.value)}
+            className="small-input"
+          />
+        </Form.Group>
         <Form.Group controlId="date">
-          <Form.Label>Select a Date</Form.Label>
+          <Form.Label>Date</Form.Label>
           <Form.Control
             type="date"
             value={date}
             onChange={(e) => setDate(e.target.value)}
-            className="w-50 small-input" // Add a custom class for smaller input
+            className="small-input"
           />
         </Form.Group>
-        <Button variant="primary" onClick={handleSubmit} className="small-button">
-          Submit
-        </Button>
+        <div className="text-center"> {/* Centered the Submit button */}
+          <Button variant="primary" onClick={handleSubmit} className="small-button mt-3">
+            Submit
+          </Button>
+        </div>
       </Form>
+      <div className="mb-3">
+        {selectedDateIndex !== null && (
+          <div className="d-flex justify-content-center">
+            <Badge variant="primary">
+              {Math.ceil(
+                (new Date(dates[selectedDateIndex].date) - new Date()) /
+                  (1000 * 60 * 60 * 24)
+              )}{' '}
+              days
+            </Badge>
+          </div>
+        )}
+      </div>
       <ListGroup>
-        {dates.map((date, index) => (
+        {dates.map((data, index) => (
           <ListGroup.Item key={index} className="d-flex align-items-center justify-content-between small-list-item">
             <Form.Check
               type="radio"
@@ -57,9 +79,10 @@ const Popup = () => {
               label=""
               onChange={() => handleSelect(index)}
               className="small-radio-button mr-2"
+              checked={selectedDateIndex === index}
             />
             <span className="small-date-text mr-2">
-              {date}
+              {data.eventName} - {data.date}
             </span>
             <Button
               variant="danger"
